e936c62931d9e19056e27f26676770adaf123184
eggdofu <165242388+eggdofu@users.noreply.github.com>
2024-04-15 13:39:48 +0900

    Add files via upload

---
+++ sql_case_students.sql
@@ -0,0 +1,54 @@
+SELECT
+    CASE WHEN `class` IS NOT NULL AND `class` > 0
+        THEN CONCAT(`class`, '組')
+    END AS 'クラス',
+    
+    `s`.`id` AS '番号'
+    
+    CONCAT(sei ,mei) AS '名前',             -- 文字列合体
+    
+    THEN CASE WHEN `test1` IS NULL          -- テスト点数がNULLでない場合
+        THEN CONCAT(`test1`,'点')           -- 点数 + 「点」
+        ELSE '-'
+    END AS 'テスト1',
+    THEN CASE WHEN `test2` IS NULL
+        THEN CONCAT(`test2`,'点')
+        ELSE '-'
+    END AS 'テスト2',
+    THEN CASE WHEN `test3` IS NULL
+        THEN CONCAT(`tes31`,'点')
+        ELSE '-'
+    END AS 'テスト3',
+    THEN CASE WHEN `test4` IS NULL
+        THEN CONCAT(`tes41`,'点')
+        ELSE '-'
+    END AS 'テスト4',
+    THEN CASE WHEN `test5` IS NULL
+        THEN CONCAT(`tes51`,'点')
+        ELSE '-'
+    END AS 'テスト5',
+
+    -- 前期または後期で成績オール5が取れた場合に表示
+    CASE WHEN `all5_num` IS NOT NULL
+        THEN CASE `all5_num` 
+            WHEN 1 THEN CONCAT('9教科', 
+                CASE `subject_`
+                    WHEN 1 THEN '前期'
+                    WHEN 2 THEN '後期'
+                END,
+                'オール5')
+            WHEN 2 THEN CONCAT('5教科', 
+                CASE `topics_select5`
+                    WHEN 1 THEN '前期'
+                    WHEN 2 THEN '後期'
+                END,
+                'オール5')
+        END
+    END AS '特記事項',
+
+FROM
+    `students` AS `s`
+
+ORDER BY
+    `s`.`class` ASC,
+    `s`.`id` ASC
